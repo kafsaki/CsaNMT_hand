642feeae2a9ca7fcda54806d97d55d5611127e89
kafsaki <1766069290@qq.com>
2024-12-28 18:27:36 +0800

    使用jieba进行分词，对分词后的句子计算WER Score

--- .ipynb_checkpoints/evaluate_WER_score-checkpoint.py
+++ .ipynb_checkpoints/evaluate_WER_score-checkpoint.py
@@ -1,14 +1,21 @@
 import json
 from jiwer import wer
 import re
+import jieba
+
+# def process_text(text):
+#     # 去掉所有标点符号
+#     text = re.sub(r'[^\w\s]', '', text)
+#     # 在每个汉字之间添加空格
+#     text = ' '.join(text)
+#     return text
 
 def process_text(text):
-    # 去掉所有标点符号
-    text = re.sub(r'[^\w\s]', '', text)
-    # 在每个汉字之间添加空格
-    text = ' '.join(text)
+    # 使用 jieba 进行分词
+    words = jieba.cut(text)
+    # 在每个词之间添加空格
+    text = ' '.join(words)
     return text
-    
 
 file_path = 'CE-CSL-translated-groundtruth.json'
 # file_path = 'CE-CSL-translated-recognition.json'
@@ -26,11 +33,15 @@ num_entries = len(data)
 for entry in data:
     translation = entry.get('translation', '')
     origin = entry.get('origin', '')
+    # print(translation)
+    # print(origin)
     translation = process_text(translation)
     origin = process_text(origin)
     # 计算 WER 分数
     score = wer(origin, translation)
     total_wer += score
+    # print(translation)
+    # print(origin)
 
 # 计算平均 WER
 average_wer = total_wer / num_entries
